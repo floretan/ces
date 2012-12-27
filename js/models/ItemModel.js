d7cfcc1078a1368a60ffdeb265afe8a64aec44ea
Florian Lorétan <florian.loretan@wunderkraut.com>
2012-12-27 21:52:20 +0000

    Set the urlRoot, not just the url.

--- js/models/ItemModel.js
+++ js/models/ItemModel.js
@@ -1,7 +1,7 @@
 
 define(['backbone'], function (Backbone) {
   return Backbone.Model.extend({
-    url: "http://ces.happypixels.com/api/log_item",
+    urlRoot: "http://ces.happypixels.com/api/log_item",
     defaults: function() {
       return {
         title: '',
