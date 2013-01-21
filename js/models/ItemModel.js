bf52c1a85bfac9b2999de7cb8ebc6e1f6b4afaf8
Florian Lorétan <florian.loretan@wunderkraut.com>
2013-01-21 16:23:20 +0000

    Fix time in the future due to time sync issues.

--- js/models/ItemModel.js
+++ js/models/ItemModel.js
@@ -2,6 +2,14 @@
 define(['backbone'], function (Backbone) {
   return Backbone.Model.extend({
     urlRoot: "http://ces.happypixels.com/api/log_item",
+    initialize: function() {
+      // Fix small inconsistencies when the client and the server 
+      // are off by a few seconds.
+      var now = new Date().getTime() / 1000;
+      if (this.get('created') > now) {
+        this.set('created', now);
+      }
+    },
     defaults: function() {
       return {
         title: '',
