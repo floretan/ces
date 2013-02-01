9297351fadca3851fb5041bd0c76355c29f0df3f
Florian Lorétan <florian.loretan@wunderkraut.com>
2013-02-01 14:57:39 +0000

    Display activity when refreshing the log view.

--- js/views/ToolbarView.js
+++ js/views/ToolbarView.js
@@ -14,9 +14,15 @@ define([
     },
     initialize: function(options) {
       this.app = options.app;
+
+      this.bindTo(this.app.itemCollection, 'reset', this.refreshComplete, this);
     },
     refresh: function() {
+      this.$('#toolbar-refresh-button i').addClass('icon-spin');
       this.app.itemCollection.fetch();
+    },
+    refreshComplete: function(e) {
+      this.$('#toolbar-refresh-button i').removeClass('icon-spin'); 
     },
     logout: function() {
       vent.trigger('user:logout');
